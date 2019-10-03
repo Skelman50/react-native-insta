a398d2dc6536c729ad4d0fab38896f4799decbd5
Skelman50 <skelman50@gmail.com>
2019-10-03 21:57:00 +0300

    add icons

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,5 +1,7 @@
 import { createAppContainer } from "react-navigation";
+import React from "react";
 import { createBottomTabNavigator } from "react-navigation-tabs";
+import { Ionicons } from "@expo/vector-icons";
 import Home from "../screens/Home";
 import Search from "../screens/Search";
 import Activity from "../screens/Activity";
@@ -7,11 +9,61 @@ import Upload from "../screens/Upload";
 import Profile from "../screens/Profile";
 
 const TabNavigator = createBottomTabNavigator({
-  Home,
-  Search,
-  Upload,
-  Activity,
-  Profile
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      tabBarLabel: " ",
+      tabBarIcon: () => {
+        return <Ionicons name={"ios-home"} size={35} />;
+      }
+    }
+  },
+  Search: {
+    screen: Search,
+    navigationOptions: {
+      tabBarLabel: " ",
+      tabBarIcon: () => {
+        return <Ionicons name={"ios-search"} size={35} />;
+      }
+    }
+  },
+  Upload: {
+    screen: Upload,
+    navigationOptions: {
+      tabBarLabel: " ",
+      tabBarIcon: ({ focused }) => {
+        return (
+          <Ionicons
+            name={focused ? "ios-add-circle" : "ios-add-circle-outline"}
+            size={35}
+          />
+        );
+      }
+    }
+  },
+  Activity: {
+    screen: Activity,
+    navigationOptions: {
+      tabBarLabel: " ",
+      tabBarIcon: ({ focused }) => {
+        return (
+          <Ionicons
+            name={focused ? "ios-heart" : "ios-heart-empty"}
+            size={35}
+          />
+        );
+      }
+    }
+  },
+  Profile: {
+    screen: Profile,
+    navigationOptions: {
+      tabBarLabel: " ",
+      tabBarIcon: () => {
+        return <Ionicons name={"ios-person"} size={35} />;
+      }
+    }
+  }
 });
 
 export default createAppContainer(TabNavigator);
